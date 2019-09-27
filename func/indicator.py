f6c4f8005b4a215fe54a8261e751ff07bfe7d1a4
jiangfeng <yxwmjf@gmail.com>
2019-09-27 22:11:56 +0800

    second commit

--- func/indicator.py
+++ func/indicator.py
@@ -43,3 +43,35 @@ def CROSS(s1,s2):
     con1 = np.where(REF(s1,1)<REF(s2,1),1,0)
     con2 = np.where(s1>s2,1,0)
     return pd.Series(con1*con2)
+
+#简单移动平均
+def MA(series,N):
+    return pd.Series.rolling(series,N).mean()
+
+#指数平滑移动平均
+def EMA(series,N):
+    res = np.nan_to_num(series).copy()
+    for i in range(1,len(series)):
+        res[i] = (res[i]*2 + res[i-1]*(N-1))/(N+1)
+    return pd.Series(res)
+
+#加权移动平均
+def SMA(series,N,M):
+    res = np.nan_to_num(series).copy()
+    for i in range(1,len(series)):
+        res[i] = (res[i]*M + res[i-1]*(N-M))/N
+    return pd.Series(res)
+
+#动态加权移动平均
+def DMA(series,A):
+    res = np.nan_to_num(series).copy()
+    for i in range(1,len(series)):
+        res[i] = A[i]*res[i] + (1-A[i])*res[i-1]
+    return pd.Series(res)
+
+#ATR指标
+def ATR(C,H,L,N):
+    TR  = MAX(MAX((H - L), ABS(REF(C, 1) - H)), ABS(REF(C, 1) - L))
+    ATR = MA(TR1,N)
+    return ATR
+
